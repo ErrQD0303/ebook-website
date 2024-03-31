a06bbb8302f9ec0108bd582f8f33d771fc82c17d
ErrQD0303 <quocdat3396@gmail.com>
2024-04-01 01:41:16 +0700

    - Fix Mobile Navigation Bar's Background - Add Formspree

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 function userScroll() {
   const navbar = document.querySelector(".navbar");
+  const navbarToggleButton = navbar.querySelector("button.navbar-toggler");
 
   window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
@@ -10,6 +11,12 @@ function userScroll() {
       navbar.classList.remove("navbar-sticky");
     }
   });
+
+  window.addEventListener("click", () => {
+    navbar.classList.toggle("bg-dark");
+  });
+
+  window.addEventListener;
 }
 
 document.addEventListener("DOMContentLoaded", userScroll);
