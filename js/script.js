74598acd283dded2f909c8787acba062a0ff0e56
ErrQD0303 <quocdat3396@gmail.com>
2024-04-01 02:29:42 +0700

    Fix Navigation Bar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 function userScroll() {
   const navbar = document.querySelector(".navbar");
   const navbarToggleButton = navbar.querySelector("button.navbar-toggler");
+  const navbarDropdown = navbar.querySelector("#navbarNavDropdown");
 
   window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
@@ -12,8 +13,14 @@ function userScroll() {
     }
   });
 
-  window.addEventListener("click", () => {
-    navbar.classList.toggle("bg-dark");
+  navbarToggleButton.addEventListener("click", (e) => {
+    if (!navbarDropdown.classList.contains("show")) {
+      navbarDropdown.classList.add("show");
+      navbar.classList.add("bg-dark");
+    } else {
+      if (window.scrollY <= 50) navbar.classList.remove("bg-dark");
+      navbarDropdown.classList.remove("show");
+    }
   });
 
   window.addEventListener;
