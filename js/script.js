0b7b95b2820c743861ef69f9a83c816a2313affa
ErrQD0303 <quocdat3396@gmail.com>
2024-04-01 12:14:49 +0700

    Fix Navigation in Mobile

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 function userScroll() {
   const navbar = document.querySelector(".navbar");
-  const navbarToggleButton = navbar.querySelector("button.navbar-toggler");
-  const navbarDropdown = navbar.querySelector("#navbarNavDropdown");
 
   window.addEventListener("scroll", () => {
     if (window.scrollY > 50) {
@@ -12,18 +10,6 @@ function userScroll() {
       navbar.classList.remove("navbar-sticky");
     }
   });
-
-  navbarToggleButton.addEventListener("click", (e) => {
-    if (!navbarDropdown.classList.contains("show")) {
-      navbarDropdown.classList.add("show");
-      navbar.classList.add("bg-dark");
-    } else {
-      if (window.scrollY <= 50) navbar.classList.remove("bg-dark");
-      navbarDropdown.classList.remove("show");
-    }
-  });
-
-  window.addEventListener;
 }
 
 document.addEventListener("DOMContentLoaded", userScroll);
